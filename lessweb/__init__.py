f3a4ec96baf503237515d990ee73d5a08842f6f8
Ji Zhang <goodhorsezxj@gmail.com>
2025-01-17 09:09:33 +0800

    feat: edit __init__.py

--- lessweb/__init__.py
+++ lessweb/__init__.py
@@ -1,4 +1,4 @@
-from .bridge import Bridge
+from .bridge import Bridge, load_module_config
 from .event import EventEmitter
 from .ioc import (Middleware, Module, Service, autowire, get_request_stack,
                   push_request_stack, rest_error, rest_response)
